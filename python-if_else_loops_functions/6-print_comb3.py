b4b2f1810617b640546d11ae29aa6eccd9cbdc78
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-05-30 09:27:14 -0700

    Fixed issue: format

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for first_digit in range(0, 10):
     for second_digit in range(first_digit + 1, 10):
         if first_digit != 8:
-            print(f"{first_digit}{second_digit}", end=", ")
+            print("{}{}".format(first_digit, second_digit), end=", ")
         else:
-            print(f"{first_digit}{second_digit}")
+            print("{}{}".format(first_digit, second_digit))
