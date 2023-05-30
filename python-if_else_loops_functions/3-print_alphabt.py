6d2f6402032d478999b170ca273e2a8bb472ed3f
vandriodd <ludasaadi@gmail.com>
2023-05-29 17:45:44 -0700

    Fixed issue: if condition

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if letter == 'e' or letter == 'q':
+    if letter == 101 or letter == 112:
         continue
     print("{}".format(chr(letter)), end="")
