10f795bb7521c26878f8392dca831d7a6795cf8b
vandriodd <ludasaadi@gmail.com>
2023-05-30 18:54:59 -0700

    Completed task: Remove at position

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    new_str = ""
-    for char in str:
-        if char == n:
-            continue
-        new_str += str[char]
-    return new_str
+    if n >= 0:
+        return str[:n] + str[n+1:]
+    return str
