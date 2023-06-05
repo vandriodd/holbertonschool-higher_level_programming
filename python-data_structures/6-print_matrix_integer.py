b60c5c0d4d0b59f703dcea2ddba224d8abab1714
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-05 10:00:58 -0700

    Fixed issue: pycodestyle and format

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for integer in row:
-            print(integer, end=" " if integer != row[-1] else "")
+        for value in row:
+            print("{:d}".format(value), end=" " if value != row[-1] else "")
         print()
