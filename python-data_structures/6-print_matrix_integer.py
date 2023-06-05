e925fcd0c440050ec14c8a59aaedd7410151a365
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-05 09:55:31 -0700

    Fixed issue: format :d

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for integer in row:
-            print("{}".format(integer), end=", " if integer != row[-1] else "")
+            print("{:d}".format(integer), end=", " if integer != row[-1] else "")
         print()
