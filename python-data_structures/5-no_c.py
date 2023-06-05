c652533c3285849a08a40391537a1bd042e512a3
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-05 08:00:16 -0700

    Fixed issue: cc

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i, letter in enumerate(my_string):
+    my_string_cpy = ""
+    for letter in my_string:
         if letter == "c" or letter == "C":
-            return my_string[:i] + my_string[i+1:]
-    return my_string
+            continue
+        my_string_cpy += letter
+    return my_string_cpy
