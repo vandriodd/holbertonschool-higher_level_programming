05ee3dcf0b6d20451b609711e99c7d32e2b09439
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-05 09:54:07 -0700

    Completed task: Lists of lists = Matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for integer in row:
+            print("{}".format(integer), end=", " if integer != row[-1] else "")
+        print()
