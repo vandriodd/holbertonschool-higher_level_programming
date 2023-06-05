1d54f04f10e205bd3eca5269fe75510a3fd363cd
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-05 09:58:57 -0700

    Fixed issue: not ,

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for integer in row:
-            print("{:d}".format(integer), end=", " if integer != row[-1] else "")
+            print(integer, end=" " if integer != row[-1] else "")
         print()
