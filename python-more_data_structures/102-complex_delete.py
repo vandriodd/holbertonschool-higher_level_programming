af569f8006054db7605192491541d01b6bb87e40
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-09 07:03:55 -0700

    Update: clean code

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    if not value:
-        return a_dictionary
-    for key, val in list(a_dictionary.items()):
-        if val is value:
-            a_dictionary.pop(key)
+    if value:
+        for key, val in list(a_dictionary.items()):
+            if val is value:
+                a_dictionary.pop(key)
     return a_dictionary
