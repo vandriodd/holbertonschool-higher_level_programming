ea448b1a9821c19bce4506a749b268fccb540e02
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-12 07:32:35 -0700

    Fixed issue: documentation added

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
+
+""" Defines Square class """
+
+
 class Square:
+    """ Initializes an square
+
+    Args:
+        size (int): size of new square
+    """
     def __init__(self, size):
         self.__size = size
