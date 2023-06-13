5ff3310fa44d79e6697fc0dcba5d7d846a0574d6
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-13 06:38:02 -0700

    Completed task: Access and update private attribute

---
+++ python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" Defines Square class """
+
+
+class Square:
+    """ Initializes an square
+
+    Args:
+        size (int): size of new square
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    """ Sets and checks if the size of the new square is valid
+
+    Args:
+        value (int): size of new square
+    """
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    """ Returns the current area of the new square """
+    def area(self):
+        return self.__size ** 2
