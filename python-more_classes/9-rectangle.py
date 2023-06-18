9d39e8c5baa38b33954df6d77d9e6c70dab5403d
vandriodd <ludasaadi@gmail.com>
2023-06-18 05:07:16 -0700

    Another test (probably doesn't work)

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -19,11 +19,11 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if height < 0:
             raise ValueError("height must be >= 0")
+        self.__height = height
         if not isinstance(width, int):
             raise TypeError("width must be an integer")
         if height < 0:
             raise ValueError("width must be >= 0")
-        self.__height = height
         self.__width = width
         Rectangle.number_of_instances += 1
 
@@ -95,4 +95,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """ Returns a new Rectangle instance """
-        return cls(size, size)
+        return Rectangle(size, size)
