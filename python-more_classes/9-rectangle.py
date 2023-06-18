e93b597bc5ec8f40fbd25de17a688c7bf9137eac
vandriodd <ludasaadi@gmail.com>
2023-06-18 01:22:29 -0700

    Fixed issue?: else

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -82,9 +82,10 @@ class Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
         if rect_1.area() >= rect_2.area():
             return rect_1
-        return rect_2
+        else:
+            return rect_2
 
     @classmethod
     def square(cls, size=0):
         """ Returns a new Rectangle instance """
-        return Rectangle(size, size)
+        return cls(size, size)
