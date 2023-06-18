f83f9449255f4257b849a8b819f454d4dd158e95
vandriodd <ludasaadi@gmail.com>
2023-06-18 01:14:31 -0700

    Fixed issue: maybe Rectangle?

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -87,4 +87,4 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """ Returns a new Rectangle instance """
-        return cls(size, size)
+        return Rectangle(size, size)
