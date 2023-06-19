7ffde290d2e6485feae845a1707c1151a5d57bc2
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-19 10:23:37 -0700

    Completed task: Same class or inherit from

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""
+    3-is_kind_of_class module
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ Checks whether the object belongs to or is an instance of the class """
+    return isinstance(obj, a_class)
