3b825782e06bd14541e64f6c8d81e563da9600d0
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-19 11:22:30 -0700

    Completed task: Improve Geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    6-base_geometry module
+"""
+
+
+class BaseGeometry:
+    """ BaseGeometry class """
+
+    def area(self):
+        """ Returns the area (not implemented) """
+        raise Exception("area() is not implemented")
