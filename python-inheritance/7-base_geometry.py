cbb11f5579f2a00874b8d8f9e060a8b141e3cd11
vandriodd <ludasaadi@gmail.com>
2023-06-20 06:27:01 -0700

    Completed task: Integer validator

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    7-base_geometry module
+"""
+
+
+class BaseGeometry:
+    """ BaseGeometry class """
+
+    def area(self):
+        """ Returns the area (not implemented) """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Function that validates a value """
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
