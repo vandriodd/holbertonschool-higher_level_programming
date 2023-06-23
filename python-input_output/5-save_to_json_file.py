21bf45411a80cfaf3d7bedf03b7cf2e4cf4611e1
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-23 06:55:22 -0700

    Completed task: Save Object to a file

---
+++ python-input_output/5-save_to_json_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+    5-save_to_json_file module
+"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Writes an Object to a text file using a JSON representation """
+    with open(filename, "w", encoding="utf-8") as file:
+        json.dump(my_obj, file)
