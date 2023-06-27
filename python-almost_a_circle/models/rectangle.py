ee0492b93c6bf5a096d42304a85bed903a181f68
Luz Saavedra <110431271+vandriodd@users.noreply.github.com>
2023-06-27 11:58:48 -0700

    Completed task: Display #1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -80,9 +80,10 @@ class Rectangle(Base):
 
     def display(self):
         """ Prints the Rectangle instance with the character # """
-        if self.__height == 0 or self.__width == 0:
-            print("")
+        for _ in range(self.y):
+            print()
         for _ in range(self.height):
+            print(" " * self.x, end="")
             print("#" * self.width)
 
     def __str__(self):
