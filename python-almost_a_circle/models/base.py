e3f3d6cdddf8de3cd199471afe486f0887e415fd
vandriodd <ludasaadi@gmail.com>
2023-06-30 07:02:22 -0700

    Completed task: JSON string to dictionary

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -38,3 +38,10 @@ class Base:
                 output_list.append(obj.to_dictionary())
         with open(filename, 'w', encoding="utf-8") as file:
             file.write(cls.to_json_string(output_list))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ Returns Python Object from JSON representation """
+        if json_string is None or len(json_string) == 0:
+            return []
+        return json.loads(json_string)
