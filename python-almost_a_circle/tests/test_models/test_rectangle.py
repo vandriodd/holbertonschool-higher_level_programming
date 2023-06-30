fb7805ba3f9f9ee497602177b4e5da1a4c22bb17
vandriodd <ludasaadi@gmail.com>
2023-06-30 16:45:54 -0700

    Add 'tests' and 'test_models' folders for unittest

---
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Unittest for Rectangle class
+"""
+import unittest
+from models.rectangle import Rectangle
+
+
+class test_rectangle(unittest.TestCase):
+    """ Test Rectangle class """
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    Unittest for Square class
+"""
+import unittest
+from models.square import Square
+
+
+class test_square(unittest.TestCase):
+    """ Test Square class """
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
