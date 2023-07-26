0f214feff6ec54d515546d0e08eb88cd673bdb12
vandriodd <ludasaadi@gmail.com>
2023-07-26 13:41:13 -0700

    Refactor code to include all code within if name == 'main' block

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -5,22 +5,22 @@ if __name__ == "__main__":
     import sys
     import MySQLdb
 
-mysql_username = sys.argv[1]
-mysql_password = sys.argv[2]
-database_name = sys.argv[3]
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-db_connection = MySQLdb.connect(
-    host='localhost',
-    user=mysql_username,
-    passwd=mysql_password,
-    database=database_name,
-    port=3306
-)
+    db_connection = MySQLdb.connect(
+        host='localhost',
+        user=mysql_username,
+        passwd=mysql_password,
+        database=database_name,
+        port=3306
+    )
 
-db_cursor = db_connection.cursor()
-db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
-rows = db_cursor.fetchall()
-for state in rows:
-    print(state)
+    db_cursor = db_connection.cursor()
+    db_cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = db_cursor.fetchall()
+    for state in rows:
+        print(state)
 
-db_connection.close()
+    db_connection.close()
