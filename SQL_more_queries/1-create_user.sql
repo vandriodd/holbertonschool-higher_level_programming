a324e93ab418c6e71d658da764ece5843e553fce
vandriodd <ludasaadi@gmail.com>
2023-07-26 05:45:56 -0700

    Delete whitespaces

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -3,5 +3,5 @@
 -- 'GRANT...' line grants all privileges of the database to user_0d_1
 -- 'FLUSH PRIVILEGES' flush privileges and reload the permission tables of the server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
