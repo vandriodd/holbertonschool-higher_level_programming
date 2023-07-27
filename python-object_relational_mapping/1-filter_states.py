dbc0936027cc37f4cb2f61cd843e9513cde14645
vandriodd <ludasaadi@gmail.com>
2023-07-27 06:26:04 -0700

    Filter states

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Module that lists all states with a name starting with N """
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
+
+    db_connection = MySQLdb.connect(
+        host='localhost',
+        user=mysql_username,
+        passwd=mysql_password,
+        database=database_name,
+        port=3306
+    )
+
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    rows = db_cursor.fetchall()
+    for state in rows:
+        print(state)
+
+    db_connection.close()
