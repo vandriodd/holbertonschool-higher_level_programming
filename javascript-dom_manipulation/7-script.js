d63df25f267ca06b60f5b50d5a1a1495f74cd2d0
vandriodd <ludasaadi@gmail.com>
2023-09-14 09:37:47 -0700

    Star Wars movies

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,21 @@
+// Script that fetches and lists all movies by given url
+
+const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
+
+fetch(url)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`${res.status}`);
+    }
+    return res.json();
+  })
+  .then(data => {
+    const movies = data.results;
+    const listMovies = document.getElementById('list_movies');
+
+    movies.map(movie => {
+      const movieElement = document.createElement('li');
+      movieElement.innerHTML = movie.title;
+      listMovies.appendChild(movieElement);
+    });
+  });
