161b2ae1b8ccb868a360b091965b068d0e89eb42
vandriodd <ludasaadi@gmail.com>
2023-09-14 13:22:55 -0700

    Refactor then response 7-script

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -4,9 +4,7 @@ const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
 
 fetch(url)
   .then(res => {
-    if (!res.ok) {
-      throw new Error(`${res.status}`);
-    }
+    if (!res.ok) throw new Error(res.status);
     return res.json();
   })
   .then(data => {
