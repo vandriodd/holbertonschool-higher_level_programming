3ce0bb664162927c239d144641cc0c8ac13538fd
vandriodd <ludasaadi@gmail.com>
2023-09-14 13:21:06 -0700

    Refactor 100-script

--- javascript-dom_manipulation/100-script.js
+++ javascript-dom_manipulation/100-script.js
@@ -1,24 +1,22 @@
 // Script that adds, removes and deletes li elements by clicking
 
-document.addEventListener('DOMContentLoaded', () => {
-  const addItem = document.getElementById('add_item');
-  const removeItem = document.getElementById('remove_item');
-  const clearList = document.getElementById('clear_list');
-  const myList = document.querySelector('.my_list');
+const addItem = document.getElementById('add_item');
+const removeItem = document.getElementById('remove_item');
+const clearList = document.getElementById('clear_list');
+const myList = document.querySelector('.my_list');
 
-  addItem.onclick = () => {
-    const newItem = document.createElement('li');
-    newItem.innerHTML = 'Item';
-    myList.appendChild(newItem);
-  };
+addItem.onclick = () => {
+  const newItem = document.createElement('li');
+  newItem.innerHTML = 'Item';
+  myList.appendChild(newItem);
+};
 
-  removeItem.onclick = () => {
-    const listItems = myList.querySelectorAll('li');
-    lastChild = listItems.length - 1;
-    myList.removeChild(listItems[lastChild]);
-  };
+removeItem.onclick = () => {
+  const listItems = myList.querySelectorAll('li');
+  lastChild = listItems.length - 1;
+  myList.removeChild(listItems[lastChild]);
+};
 
-  clearList.onclick = () => {
-    myList.innerHTML = '';
-  };
-});
+clearList.onclick = () => {
+  myList.innerHTML = '';
+};
