262bfe26cb285a426d7ea947003eb57b2335c40f
vandriodd <ludasaadi@gmail.com>
2023-09-14 09:16:04 -0700

    Star wars character

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,16 @@
+// Script that fetches a character from given URL
+
+const url = 'https://swapi-api.hbtn.io/api/people/5/?format=json';
+
+fetch(url)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`Sure about that URL? (${res.status})`);
+    }
+    return res.json();
+  })
+  .then(data => {
+    const characterElement = document.getElementById('character');
+    let charName = data.name;
+    characterElement.innerHTML = charName;
+  });
