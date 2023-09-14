1cc5c184d22b805dbbb89d6245365e233a4fefdd
vandriodd <ludasaadi@gmail.com>
2023-09-14 09:47:13 -0700

    Say Hello!

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,15 @@
+// Script that fetches and displays hello from given url in #hello
+
+const url = 'https://hellosalut.stefanbohacek.dev/?lang=fr';
+
+fetch(url)
+  .then(res => {
+    if (!res.ok) {
+      throw new Error(`${res.status}`);
+    }
+    return res.json();
+  })
+  .then(data => {
+    const helloElement = document.getElementById('hello');
+    helloElement.innerHTML = data.hello;
+  });
