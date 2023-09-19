764b1b4ab82a8af15c3581d07b0c41ff8c9b81f6
vandriodd <ludasaadi@gmail.com>
2023-09-19 13:26:55 +0000

    Remove console.log

--- javascript-web_jquery/8-script.js
+++ javascript-web_jquery/8-script.js
@@ -1,15 +1,14 @@
 const url = 'https://swapi-api.hbtn.io/api/films/?format=json';
 
 $.ajax({
-	url: url,
-	success: (data) => {
-		const movies = data.results;
-		const listMovies = $('#list_movies');
-		$.map(movies, (movie) => {
-			let movieTitle = movie.title;
-			console.log(movieTitle);
-			let movieElement = $('<li>').text(movieTitle);
-			movieElement.appendTo(listMovies);
-		});
-	},
+  url: url,
+  success: (data) => {
+    const movies = data.results;
+    const listMovies = $('#list_movies');
+    $.map(movies, (movie) => {
+      let movieTitle = movie.title;
+      let movieElement = $('<li>').text(movieTitle);
+      movieElement.appendTo(listMovies);
+    });
+  }
 });
